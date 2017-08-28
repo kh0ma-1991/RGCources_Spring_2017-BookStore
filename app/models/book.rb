6d5b6cc78ac917e0e9794835389848942278a366
kh0ma-1991 <kh0ma@yahoo.com>
2017-08-28 15:44:35 +0300

    prevent duplication in book authors

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
-  has_and_belongs_to_many :authors
+  has_and_belongs_to_many :authors, distinct: true do
+    # prevent duplication in db
+    def << (author)
+      author -= self if author.respond_to?(:to_a)
+      super author unless include?(author)
+    end
+  end
 end
