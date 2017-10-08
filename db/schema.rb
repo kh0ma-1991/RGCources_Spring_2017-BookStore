9c8838140e36ac9191e5aa131c88e4ad7a9a0b22
kh0ma-1991 <kh0ma@yahoo.com>
2017-10-08 23:42:14 +0300

    bin/rails g migration AddSubtotalToOrders subtotal:'decimal{12,3}'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001184705) do
+ActiveRecord::Schema.define(version: 20171008204009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20171001184705) do
     t.decimal "total_price", precision: 12, scale: 3
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "subtotal", precision: 12, scale: 3
   end
 
   create_table "pictures", force: :cascade do |t|
