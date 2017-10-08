9654340e6920d2e62c6b4a05898f8d83fdd13ca5
kh0ma-1991 <kh0ma@yahoo.com>
2017-10-08 23:53:33 +0300

    bin/rails g migration SetDefaultValueQuantityForOrderItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008204009) do
+ActiveRecord::Schema.define(version: 20171008204758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 20171008204009) do
   create_table "order_items", force: :cascade do |t|
     t.bigint "book_id"
     t.bigint "order_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 0, null: false
     t.decimal "total_price", precision: 12, scale: 3
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
