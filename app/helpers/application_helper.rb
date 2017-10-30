f461a34ff44ab68020e3c2e85fbab79c0f1ed3b7
kh0ma-1991 <kh0ma@yahoo.com>
2017-10-30 18:49:11 +0200

    begin with complete #9

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,10 @@ module ApplicationHelper
     delivery = @order.delivery
     delivery ? delivery.price : 0
   end
+
+  def order_code order
+    id = order.id.to_s
+    return '#R'+id if id.length >= 9
+    '#R00000000'.slice(0,10-id.length)+id
+  end
 end
